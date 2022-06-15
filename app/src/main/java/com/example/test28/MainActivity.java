e590a361f3b1b846cae94a665d23268d4d0d1de5
HamsterForest <republic7788@gmail.com>
2022-06-15 11:19:49 +0900

    6_15_1Commit

--- app/src/main/java/com/example/test28/MainActivity.java
+++ app/src/main/java/com/example/test28/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = getIntent();
-        String str = intent.getExtras().getString("키");
+        Intent intent = getIntent();//값을 받기 위함, subactivity에서 받는다.
+        String str = intent.getExtras().getString("키");//string형식으로 넘겨줬으니까
         Log.d("로그",str);
+        EditText editText=findViewById(R.id.edit_text);
+        editText.setText(str);
     }
 }
